07f6ad80d34438d6cfd77eedb2ea08c3ac63d82d
tamaslud <tamaslud@yahoo.com>
2022-08-25 16:50:12 +0200

    display and clear numbers

--- js/calculator.js
+++ js/calculator.js
@@ -1,26 +1,67 @@
 'use strict'
-const allButtons = document.querySelectorAll('div.calculator .btn');
+
+const numButtons = document.querySelectorAll('div.calculator .btn-nums');
+const operatorButtons = document.querySelectorAll('div.calculator .btn-operator');
+const equalsButton = document.querySelectorAll('div.calculator .btn-equals');
+const clearButton = document.querySelectorAll('div.calculator .btn-clear');
+const decimalButton = document.querySelectorAll('div.calculator .btn-decimal');
+
 let accu = '';
 let enabledButtons = ['0','1','2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/', 'c', '=']
 const displayNumbers = document.querySelector('.displayDigits');
 
-
 const clearScreen = () => {
-    
-}
-
-const addButtonListener = () => {    
-    for (let i = 0; i < allButtons.length; i += 1) {
-         allButtons[i].addEventListener("click", () => 
-         {buttonEventHandler(allButtons[i].getAttribute("data-btn"))}
-         );
-
-    };
+        accu = '';
+        displayNumbers.innerHTML='0';
     }
+
+const addDecimal = () => {
+    accu += '.'
+    displayNumbers.innerHTML=accu;
+    removeDecimalButtonListener();
+}
+// Button listeners:
+const addClearButtonListener = () => {
+        clearButton[0].addEventListener("click", () => clearScreen());
+   };
+
+
+
+const addDecimalButtonListener = () => {
+        decimalButton[0].addEventListener("click", () => addDecimal());
+   };
+const removeDecimalButtonListener = () => {
+        decimalButton[0].removeEventListener("click", () => addDecimal());
+   };
+
+
+
+
+const addOperatorButtonsListener = () => {    
+    for (let i = 0; i < operatorButtons.length; i += 1) {
+         operatorButtons[i].addEventListener("click", () => 
+         buttonEventHandler(operatorButtons[i].getAttribute("data-btn"))
+         );
+        };
+    };
+
+
+const addNumButtonsListener = () => {    
+    for (let i = 0; i < numButtons.length; i += 1) {
+         numButtons[i].addEventListener("click", () => 
+         {buttonEventHandler(numButtons[i].getAttribute("data-btn"));}
+         );
+        };
+    //addOperatorButtonsListener();
+    };
+
 
 const buttonEventHandler = (button) => {
     accu += button
     displayNumbers.innerHTML=accu;
 }
 
-addButtonListener();
+clearScreen();
+addClearButtonListener();
+addDecimalButtonListener();
+addNumButtonsListener();
