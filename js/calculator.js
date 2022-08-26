d95da32df31b4cb59f4110fe46e7c7b3723d641b
tamaslud <tamaslud@yahoo.com>
2022-08-26 17:50:28 +0200

    calculation ready

--- js/calculator.js
+++ js/calculator.js
@@ -6,7 +6,7 @@ const equalsButton = document.querySelectorAll('div.calculator .btn-equals');
 const clearButton = document.querySelectorAll('div.calculator .btn-clear');
 const decimalButton = document.querySelectorAll('div.calculator .btn-decimal');
 
-let stringToDisplay;
+let stringToDisplay = '';
 let lastOperand;
 let accu;
 let enabledButtons;
@@ -34,21 +34,23 @@ const addDecimal = () => {
 }
 
 const numButtonEventHandler = (button) => {
-    if (button =='0' && lastOperand == '') {}else{
+    if (button =='0' && lastOperand == '0') {}else{
         enabledButtons = enabledButtons.concat(['0', '+', '-', 'x', '/']);
         if (lastOperand == '0') {
             lastOperand = '';
             stringToDisplay = stringToDisplay.slice(0, -1);
         }
-        stringToDisplay += button;
-        lastOperand += button;
-        displayNumbers.innerHTML=stringToDisplay;
+        if (lastOperand.length <10){
+            stringToDisplay += button;
+            lastOperand += button;
+            displayNumbers.innerHTML=stringToDisplay;
+        }
     }
 }
 
 const operatorButtonEventHandler = (button) => {
     if (enabledButtons.includes(button)){
-        stringToDisplay += button;
+        stringToDisplay += ` ${button} `;
         lastOperand = '';
         displayNumbers.innerHTML=stringToDisplay;
         enabledButtons.push('.');
@@ -57,9 +59,45 @@ const operatorButtonEventHandler = (button) => {
 }
 
 const calcResult = () => {
-    stringToDisplay += '=';
-    displayNumbers.innerHTML=stringToDisplay;
+    let blocks = stringToDisplay.split(' ');
+    
+    if (lastOperand !== '' && blocks.length > 1) {
+        let operandA = blocks.shift();
+        while (blocks.length > 1) {
+            let operator = blocks.shift();
+            let operandB = blocks.shift();
+            operandA = evaluate(operandA, operator, operandB);
+        }
+        stringToDisplay = operandA;
+        lastOperand = operandA;
+        displayNumbers.innerHTML=stringToDisplay;
+        enabledButtons = ['+', '-', 'x', '/', 'c', '='];
+    }
 }
+
+const evaluate = (a, op, b) => {
+    let result = ''
+    switch(['+', '-', 'x', '/'].indexOf(op)) {
+        case 0:
+          result = Number(a) + Number(b);
+          break;
+        case 1:
+          result = Number(a) - Number(b);
+          break;
+        case 2:
+          result = Number(a) * Number(b);
+          break;
+        case 3:
+          result = Number(a) / Number(b);
+          break;
+        default:
+            result = 'error'
+        }
+
+    return (Math.round(result*10000000)/10000000).toString();
+    
+}
+
 
 // Button listeners:
 const addClearButtonListener = () => {
